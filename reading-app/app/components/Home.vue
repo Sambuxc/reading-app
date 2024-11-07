16f9b8b1800261d70ccea4d18561a1615f1f593f
Samuel Bruton <samuel.bruton@ca.vu>
2024-11-07 16:03:33 +0000

    app home renders list view of data

--- reading-app/app/components/Home.vue
+++ reading-app/app/components/Home.vue
@@ -1,43 +1,52 @@
 <template>
   <Page>
-    <ActionBar>
-      <Label text="Home"/>
-    </ActionBar>
+    <ActionBar title="NativeFlix" />
 
-    <GridLayout>
-      <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; "/>
-          <Span :text="message"/>
-        </FormattedString>
-      </Label>
-    </GridLayout>
+    <ListView height="100%" separatorColor="transparent" for="item in flicks">
+      <v-template>
+        <GridLayout
+          height="280"
+          borderRadius="10"
+          class="bg-secondary"
+          rows="*, auto, auto"
+          columns="*"
+          margin="5 10"
+          padding="0"
+        >
+          <Image row="0" margin="0" stretch="aspectFill" :src="item.image" />
+          <Label
+            row="1"
+            margin="10 10 0 10"
+            fontWeight="700"
+            class="text-primary"
+            fontSize="18"
+            :text="item.title"
+          />
+          <Label
+            row="2"
+            margin="0 10 10 10"
+            class="text-secondary"
+            fontSize="14"
+            textWrap="true"
+            :text="item.description"
+          />
+        </GridLayout>
+      </v-template>
+    </ListView>
   </Page>
 </template>
 
 <script lang="ts">
-  import Vue from "nativescript-vue";
+import Vue from 'nativescript-vue'
+import FlickService from '../services/FlickService'
 
-  export default Vue.extend({
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
+const flickService = new FlickService()
+
+export default Vue.extend({
+  data() {
+    return {
+      flicks: flickService.getFlicks()
     }
-  });
+  }
+})
 </script>
-
-<style scoped lang="scss">
-  @import '@nativescript/theme/scss/variables/blue';
-
-  // Custom styles
-  .fas {
-    @include colorize($color: accent);
-  }
-
-  .info {
-    font-size: 20;
-    horizontal-align: center;
-    vertical-align: center;
-  }
-</style>
